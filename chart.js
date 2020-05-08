9dfa4a1f87f48eec90e176e38fb46cee3b79e388
jason yang <syan19@live.cn>
2020-05-08 22:55:01 +1000

    site finished color needs to adjusted

---
+++ chart.js
@@ -0,0 +1,27 @@
+var ctx = document.getElementById("myChart").getContext("2d");
+var myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: ["rgba(255, 255, 255, 0.2)"],
+        borderColor: ["rgba(255, 0, 84, .75)"],
+        borderWidth: 3,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});

3f1c3dbbbb817ea5302baf5f34ceb46badcee54a
jason yang <syan19@live.cn>
2020-05-08 23:17:46 +1000

    site finished

--- chart.js
+++ chart.js
@@ -8,7 +8,7 @@ var myChart = new Chart(ctx, {
         label: "# of Votes",
         data: [12, 19, 3, 5, 2, 3],
         backgroundColor: ["rgba(255, 255, 255, 0.2)"],
-        borderColor: ["rgba(255, 0, 84, .75)"],
+        borderColor: ["rgba(255, 230, 109,1)"],
         borderWidth: 3,
       },
     ],
